4ed94198ee0e2c4d282da83418dc7bc4a6aa29ae
Hugo Blom <bl0m1@users.noreply.github.com>
2020-08-16 14:35:41 +0200

    Update run-me.sh

--- run-me.sh
+++ run-me.sh
@@ -24,39 +24,32 @@ echo "Selected Folder: $remotefolder";
 
 while true
 do
-  if [ ! -f $lastrunf ]; then
-    echo 0 > $lastrunf;
-  fi
-
   now=$(date +%s);
-  lastrun=`cat $lastrunf`;
-  diff=$(( $now - $lastrun ));
+  echo $now > $lastrunf;
+  if [ ! -f ${lock} ]; then
+    echo "Running rclone move"
+    # create a lockfile containing PID...
+    # rclone can take a while. This keeps multiple instances from running.
+    echo "$$" > ${lock}
 
-  if [ $diff -gt 3599 ];
-  then
-      echo "Running rclone move"
-      echo $now > $lastrunf;
-      if [ ! -f ${lock} ]; then
-              # create a lockfile containing PID...
-              # rclone can take a while. This keeps multiple instances from running.
-              echo "$$" > ${lock}
+    echo "-> Starting rclone move -> drive"
+    rclone move $local $remotedrive:$remotefolder --config "/config/rclone.conf" --checksum --delete-after  --tpslimit 8 --tpslimit-burst 1  --transfers 10 --min-age 60m -v --bwlimit 9M
+    echo "<- Finished rclone move -> drive"
 
-              echo "-> Starting rclone move -> drive"
-              rclone move $local $remotedrive:$remotefolder --config "/config/rclone.conf" --checksum --delete-after  --tpslimit 8 --tpslimit-burst 1  --transfers 10 --min-age 60m -v --bwlimit 9M
-              echo "<- Finished rclone move -> drive"
+    echo "--------------------------------"
 
-              echo "--------------------------------"
+    # remove lock.
+    rm ${lock}
+    
+    # Sleep
+    sleep 60
 
-              # remove lock.
-              rm ${lock}
-
-              # success!
-      else
-              # error!
-              echo "Update already running on PID $(cat ${lock})."
-      fi
+    # success!
   else
-      echo "less then one hour, trying again soon";
-      sleep 60;
-  fi;
+    # error!
+    echo "Update already running on PID $(cat ${lock})."
+    
+    # Sleep
+    sleep 60
+  fi
 done
